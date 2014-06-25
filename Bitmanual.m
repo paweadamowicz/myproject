be4234f159d8c5d3a58bb8c8f1ef38016b7287f2
paweadamowicz <paweadamowicz@gmail.com>
2014-06-25 11:25:20 +0200

    Update3
    adding a comment

--- Bitmanual.m
+++ Bitmanual.m
@@ -1,6 +1,9 @@
 clc
 clear all
+%type you code instead of 0,1,0,1,1,1,1,0,1,0,0. Between each 0 and 1 type
+%coma (,).
 bitstream=[0,1,0,1,1,1,1,0,1,0,0];
+%DO NOT EDIT
 length(bitstream)
 t=0:(length(bitstream)-1);
 figure;
